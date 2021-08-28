c5e31d5a2d87b888835b37143ae2e915c1df2b20
TDRAR <88198905+TDRAR@users.noreply.github.com>
2021-08-28 19:50:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() 
+{
+  createCanvas(400,400);
+background("yellow")
+box=createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+if(keyIsDown(RIGHT_ARROW)){
+background("black")
+}
+drawSprites()
+}
+
+
+
+
